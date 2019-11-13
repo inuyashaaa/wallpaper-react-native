a73cfcf43f3d461c5d4a22123f492dd8f9cc4410
Phạm Huy Mạnh <huymanhtmhp@gmail.com>
2019-11-13 22:38:41 +0700

    Fix bug

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import {
 } from 'react-native'
 import Unsplash, { toJson } from 'unsplash-js'
 import WallPaperManager from 'react-native-wallpaper-manager'
+// import FastImage from 'react-native-fast-image'
 
 const { width } = Dimensions.get('window')
 
@@ -22,17 +23,16 @@ const App = () => {
         '13ff6331fa5d44c2fff1bade0ab68eda4946dc5be59648e9730b219fd219ccb8',
     })
     unsplash.search
-      .photos('dogs', 1, 1000, { orientation: 'portrait' })
+      .photos('dogs', 1, 100, { orientation: 'portrait' })
       .then(toJson)
       .then((res) => {
-        console.log('res', res)
         setListImage(res.results)
       })
 
     WallPaperManager.setWallpaper(
       {
         uri:
-          'https://images.pexels.com/photos/2974657/pexels-photo-2974657.jpeg',
+          'https://images.pexels.com/photos/3178881/pexels-photo-3178881.jpeg',
       },
       res => console.log(res),
     )
@@ -43,6 +43,7 @@ const App = () => {
   return (
     <View style={{ flex: 1 }}>
       <FlatList
+        showsVerticalScrollIndicator={false}
         data={listImage}
         numColumns={3}
         keyExtractor={(item, index) => `Image-${index}`}
